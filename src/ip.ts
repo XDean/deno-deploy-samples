80b057477d0060fca2c01b0e71574b8846420341
dxu <dean.xu@asml.com>
2021-03-31 17:25:03 +0800

    add ip.ts

---
+++ src/ip.ts
@@ -0,0 +1,9 @@
+addEventListener("fetch", (event) => {
+  // Get the value of the header named `x-forwarded-for`.
+  // The value can either be a IPv4 or IPv6 address.
+  const ip = event.request.headers.get("x-forwarded-for");
+  const response = new Response(`Your IP address is <b>${ip}</b>`, {
+    headers: { "content-type": "text/html" },
+  });
+  event.respondWith(response);
+});
