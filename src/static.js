8c9d752a23e41391706215410a03e0fc66b68c5a
Dean Xu <373216024@qq.com>
2021-03-31 19:30:22 +0800

    update static

--- src/static.js
+++ src/static.js
@@ -1,37 +1,14 @@
-console.log(Deno)
 async function handleRequest(request) {
-  const {pathname} = new URL(request.url)
-  const filepath = '.' + pathname.replaceAll('_/', '../')
-  const stat = await Deno.stat(filepath).catch(() => ({isFile: false, isDirectory: false}))
-  if (stat.isFile) {
-    const content = await Deno.readTextFile(pathname.substring(1)).catch(e => 'fail to load file: ' + e.toString())
-    return new Response(content, {
-      headers: {
-        'Content-Type': 'text/plain; charset=utf-8',
-      }
-    })
-  } else if (stat.isDirectory) {
-    const entries = []
-    for await (const dirEntry of Deno.readDir(filepath)) {
-      entries.push(dirEntry);
-    }
-    return new Response(`
-<div>
-<h2>${filepath}</h2>
-<ul class="line-height:2; font-size:1.5rem">
-<li><a href="_/">..</a></li>
-${entries.map(e => {
-      const link = e.name + (e.isDirectory ? '/' : '')
-      return `<li><a href="${link}">${link}</a></li>`;
-    }).join('\n')}
-</ul>
-</div>`, {
-      headers: {
-        'Content-Type': 'text/html; charset=utf-8',
-      }
-    })
-  } else {
-    return new Response('not found', {status: 404})
+  const path = new URL(request.url).pathname.substring(1)
+  return new Response(await readURL(path), {status: 200})
+}
+
+async function readURL(path) {
+  const base = import.meta.url
+  if (base.startsWith('file')) {
+    return Deno.readTextFile(path).catch(e => 'fail to load file: ' + e.toString())
+  } else if (base.startsWith('http')) {
+    return fetch(new URL(path, import.meta.url).toString()).then(e => e.body)
   }
 }
 
